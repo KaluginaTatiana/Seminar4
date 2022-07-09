8cc1c242589d4b9ec7dfbfb5a2b73b1407a7e5da
Tatiana <ttt-ttt@bk.ru>
2022-07-10 02:17:30 +0300

    Tasks of seminar 4

---
+++ Task24/Program.cs
@@ -0,0 +1,24 @@
+﻿// Принимает на вход число N и выдает на выходе сумму чисел от 1 до N
+Console.WriteLine("Ведите целое положительное число.");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n > 0 && n % 1 ==0)
+{
+    Console.WriteLine("Сумма чисел от 1 до " + n + " равна " + FindSum(n));
+}
+else
+{
+    Console.WriteLine("Ошибка ввода. Введите целое положительное число.");
+}
+
+int FindSum(int number)
+{
+    int sum = 0;
+    for (int i = 1; i <= number; i++)
+    {
+        sum = sum + i;
+    }
+    return sum;
+}
+
+
+
